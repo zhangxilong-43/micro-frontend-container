dd0ede9dc2517780349435d1197af32dd52116e3
zhangxilong01 <zhangxilong01@qianxin.com>
2024-06-10 22:46:16 +0800

    feat: 增加容器默认页

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './style/App.less';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, redirect } from 'react-router-dom';
 import MicroApps from './Components/MicroApps';
 import Menu from './Components/Menu';
-import Error from './Components/error';
+import Error from './Components/Error';
+import Container from './Components/Container';
 
 export default function App() {
     return (
@@ -11,7 +12,7 @@ export default function App() {
             <BrowserRouter>
                 <Menu />
                 <Routes>
-                    <Route path="/" element={<></>} />
+                    <Route path="/" element={<Container />} />
                     <Route path="/micro-apps/:microAppId" element={<MicroApps />} />
                     <Route path="*" element={<Error />} />
                 </Routes>
---
+++ src/Components/Container.jsx
@@ -0,0 +1,6 @@
+import React  from "react";
+import '../style/Container.less';
+
+const Container = () => <h1 className='container-info'>Hello, I'm Container.</h1>
+
+export default Container;
